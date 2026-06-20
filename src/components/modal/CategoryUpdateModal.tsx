a2b1eb47651d006a3c347dca2c6d46b422db1723
Amit <amitmohammad46@gmail.com>
2026-06-21 03:38:56 +0600

    fix: tutor category edit button remove

--- src/components/modal/CategoryUpdateModal.tsx
+++ src/components/modal/CategoryUpdateModal.tsx
@@ -8,45 +8,31 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
 import { Category } from "@/types/category.types";
 import { useState, useTransition } from "react";
 import { updateCategory } from "@/actions/category.action";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 const CategoryUpdateModal = ({
   isOpen,
   setIsOpen,
   category,
-  allcategories,
 }: {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
   category: Category;
-  allcategories: Category[];
 }) => {
   const { id, name } = category;
-  const [categoryId, setCategoryId] = useState(name);
+  const [value, setValue] = useState(name);
   const [isPending, startTransition] = useTransition();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    const selectedCategory = allcategories.find((k) => k.id === categoryId);
-    if (!selectedCategory) return;
-
     startTransition(async () => {
       try {
-        const result = await updateCategory(selectedCategory.id, selectedCategory.name);
-        console.log(result);
+        const result = await updateCategory(id, value);
         if (result?.success) {
           toast.success("Category updated successfully", {
             position: "top-right",
@@ -69,25 +55,15 @@ const CategoryUpdateModal = ({
         </DialogHeader>
 
         <form>
-          <Label htmlFor="category" className="pb-2">
+          <Label htmlFor="name" className="pb-2">
             Name
           </Label>
-
-          <Select value={categoryId} onValueChange={setCategoryId}>
-            <SelectTrigger className="w-full">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent className="w-full">
-              <SelectGroup>
-                {allcategories.map((k) => (
-                  <SelectItem key={k.id} value={k.id}>
-                    {k.name}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-
+          <Input
+            id="name"
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
           <DialogFooter className="pt-4">
             <DialogClose asChild>
               <Button variant="outline" className="cursor-pointer">
